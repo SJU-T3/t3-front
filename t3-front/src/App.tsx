bd2a4a8499a8dda599d220815a94f1b7c32e2d1f
koo-eunseo <kooes2003@naver.com>
2025-12-01 18:07:15 +0900

    라우터 추가

--- t3-front/src/App.tsx
+++ t3-front/src/App.tsx
@@ -7,6 +7,13 @@ import ChattingPage from "./pages/chatting/ChattingPage";
 import LoginPage from "./pages/Login/LoginPage";
 import IncomeDetailPage from "./pages/IncomeDetailPage";
 import CategoryDetailPage from "./pages/CategoryDetailPage";
+import AddPage from "./pages/AddPage";
+import ChattingListPage from "./pages/chatting/ChattingListPage";
+import UpdateGoalPage from "./pages/UpdateGoalPage";
+import UpdateTargetAmountPage from "./pages/UpdateTargetAmountPage";
+import ChangeCharacterPage from "./pages/chatting/UpdateCharacterPage";
+import CreateNewChattingPage from "./pages/chatting/CreateNewChattingPage";
+import NewChattingPage from "./pages/chatting/NewChattingPage";
 function App() {
   return (
     <BrowserRouter>
@@ -20,6 +27,17 @@ function App() {
           <Route path="/income" element={<IncomeDetailPage />} />
           <Route path="/chatting" element={<ChattingPage />} />
           <Route path="/report/category/:categoryName" element={<CategoryDetailPage />} />
+          <Route path="/chatting/:roomId" element={<ChattingPage />} />
+          <Route path="/chatting-list" element={<ChattingListPage />} />
+          <Route path="/add" element={<AddPage />} />
+          <Route path="/update-goal" element={<UpdateGoalPage />} />
+          <Route path="/update-amount" element={<UpdateTargetAmountPage />} />
+          <Route
+            path="/chatting/:roomId/update-character"
+            element={<ChangeCharacterPage />}
+          />
+          <Route path="/chatting/new" element={<CreateNewChattingPage />} />
+          <Route path="/chatting/new/message" element={<NewChattingPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
